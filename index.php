d67d65e641f756721866fd92691b2ea59b22a312
Reza Mohiti <rm.biqarar@gmail.com>
2018-03-03 11:47:04 +0330

    fix

--- index.php
+++ index.php
@@ -1,20 +1,12 @@
 <?php
-require_once ("num2word.php");
-$num2word = new Num2word;
-echo $num2word->convert(123);
-echo "<br>";
-echo $num2word->convert(3000000020);
-echo "<br>";
-echo $num2word->convert(54654564);
-echo "<br>";
-echo $num2word->convert(-1545);
-echo "<br>";
-echo $num2word->convert(0);
-echo "<br>";
-echo $num2word->convert(0000000001);
-echo "<br>";
-echo $num2word->convert(9999999999999999);
-echo "<br>";
-echo $num2word->convert('abc');
-echo "<br>";
+require_once ("Num2word.php");
+$Num2word = new \Num2word;
+echo $Num2word->convert(123);
+echo $Num2word->convert(3000000020);
+echo $Num2word->convert(54654564);
+echo $Num2word->convert(-1545);
+echo $Num2word->convert(0);
+echo $Num2word->convert(0000000001);
+echo $Num2word->convert(9999999999999999);
+echo $Num2word->convert('abc');
 ?>
